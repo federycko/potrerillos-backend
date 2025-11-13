e230e7e04436f197ccce8b03ab55d359886df018
Federico Gallo <federycko@yahoo.com.ar>
2025-11-13 20:38:42 -0300

    vite conf

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true,
+    port: 1337,
+    strictPort: true,
     allowedHosts: [
       'localhost',
       '127.0.0.1',
+      'raspberrypi',
       'raspberrypi.bison-algol.ts.net',
     ],
+    cors: true,
   },
 });
