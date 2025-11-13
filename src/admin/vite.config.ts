1d054f44ae885cb14f1f47ca609710527ffa0c93
Federico Gallo <federycko@yahoo.com.ar>
2025-11-13 20:50:49 -0300

    vite conf

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
+import type { ViteUserConfig } from '@strapi/types';
 
-export default defineConfig({
+export default defineConfig((config: ViteUserConfig) => ({
+  ...config,
   server: {
+    ...config.server,
     host: true,
-    port: 1337,
-    strictPort: true,
     allowedHosts: [
       'localhost',
       '127.0.0.1',
       'raspberrypi',
       'raspberrypi.bison-algol.ts.net',
     ],
-    cors: true,
   },
-});
+}));
