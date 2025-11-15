6a8b2ce5c969f81ab8a27597d94e66aa812940f3
Federico Gallo <federycko@yahoo.com.ar>
2025-11-15 15:35:23 -0300

    config strapi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -14,6 +14,7 @@ export default [
             'blob:',
             'dl.airtable.com',
             'raspberrypi.bison-algol.ts.net',
+            'https://potrerillos-frontend.pages.dev',
           ],
           'media-src': [
             "'self'",
@@ -21,13 +22,29 @@ export default [
             'blob:',
             'dl.airtable.com',
             'raspberrypi.bison-algol.ts.net',
+            'https://potrerillos-frontend.pages.dev',
           ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://potrerillos-frontend.pages.dev',  // Tu URL de Cloudflare Pages
+        'http://localhost:3000',                    // Desarrollo local
+        'http://localhost:3001',                    // Desarrollo local alternativo
+        // Producción futura:
+        // 'https://diquepotrerillos.com.ar',
+        // 'https://www.diquepotrerillos.com.ar',
+      ],
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
